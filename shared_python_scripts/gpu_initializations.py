c7a7ff2d542372e399793d86d0ac75c0f51e2e37
Emmanouil Gionanidis <gionanidis32@gmail.com>
2019-06-29 14:12:57 +0300

    Update gpu_initializations.py

--- shared_python_scripts/gpu_initializations.py
+++ shared_python_scripts/gpu_initializations.py
@@ -23,10 +23,22 @@ def CUDA_init(core,memory):
         
         if(core == "GPU"):
         
-                #define which GPU to use, the number is the ID of the GPU given from your system (bus ID)
-                os.environ["CUDA_VISIBLE_DEVICES"]="0"
+        
+                if(parallel):
+        
+                        #running the example on multiple gpus
+                        os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+                        
+                        print('\n ----------------- Using multiple GPUs ----------------- \n')
+                        
+                else:
+        
+                        #define which GPU to use, the number is the ID of the GPU given from your system (bus ID)
+                        os.environ["CUDA_VISIBLE_DEVICES"]="0"
                 
-                print('\n ----------------- Using GPU ----------------- \n')
+                
+                        print('\n ----------------- Using GPU ----------------- \n')
+                        
         
         elif(core == "CPU"):
         
