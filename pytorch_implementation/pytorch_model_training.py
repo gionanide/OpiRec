214f4c5940ced010cc29a5073d8033499fbdb146
Emmanouil Gionanidis <gionanidis32@gmail.com>
2019-09-04 15:19:54 +0300

    Update pytorch_model_training.py

--- pytorch_implementation/pytorch_model_training.py
+++ pytorch_implementation/pytorch_model_training.py
@@ -1,1 +1,8 @@
-
+#!usr/bin/python
+from pytorch_models import recommender_v1_encoder
+from pytorch_models import recommender_v1_decoder
+import preprocessing
+import torch
+import evaluation
+import sys
+import word2vec
